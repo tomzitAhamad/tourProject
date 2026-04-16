723f14a633bd213b62d2aa07f61281f23aa7701f
tomzit <barisakib6@gmail.com>
2026-04-16 15:01:42 +0600

    Added package selection using session and redirect to register page

--- set_package.php
+++ set_package.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
-
 if(isset($_GET['package'])){
-    $_SESSION['selected_package'] = $_GET['package']; // 'coxsbazar', 'sajek', or 'sylhet'
-    header("Location: register.php"); // redirect to registration/login
+    $_SESSION['selected_package'] = $_GET['package']; 
+    header("Location: register.php"); 
     exit();
 } else {
     header("Location: index.html");
