e2c4446b148b492e85b9542e1392dc43b1bd07fa
tomzit <barisakib6@gmail.com>
2026-04-19 00:18:37 +0600

    added booking system

---
+++ booking_success.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Success</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<section class="register_section_class">
+    <div class="form_container_class">
+
+        <h2>🎉 Booking Successful!</h2>
+        <p>Your tour package has been booked successfully.</p>
+
+        <a href="index.html" class="btn_primary_class">Go Home</a>
+
+    </div>
+</section>
+
+</body>
+</html>
