fe63a78c1e69df45d187694729fcc683089dc3d6
tomzit <barisakib6@gmail.com>
2026-04-20 00:22:24 +0600

    updated data insertion

--- insert.php
+++ insert.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 
 include 'connection.php';
 
-// Collect user info safely
+
 $fname = mysqli_real_escape_string($con, $_POST['fname']);
 $lname = mysqli_real_escape_string($con, $_POST['lname']);
 $username = mysqli_real_escape_string($con, $_POST['uname']);
@@ -12,13 +12,13 @@ $email = mysqli_real_escape_string($con, $_POST['email']);
 $mobile = mysqli_real_escape_string($con, $_POST['mobile']);
 $password = mysqli_real_escape_string($con, $_POST['pass']);
 
-// Insert user
+
 $sql_user = "INSERT INTO users(fname,lname,username,email,mobile,password) 
              VALUES('$fname','$lname','$username','$email','$mobile','$password')";
 
 if(mysqli_query($con, $sql_user)){
     echo "<script>
-            alert('Registration Successful ✅');
+            alert('Registration Successful ');
             window.location.href='login.php';
           </script>";
 }else{
