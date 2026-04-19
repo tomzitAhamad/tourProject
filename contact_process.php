595c2b089dbb715bb1d43b2ee678eae7ae4a87a1
tomzit <barisakib6@gmail.com>
2026-04-19 12:24:23 +0600

    user feedback

---
+++ contact_process.php
@@ -0,0 +1,19 @@
+<?php
+include 'db_connect.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $subject = mysqli_real_escape_string($conn, $_POST['subject']);
+    $message = mysqli_real_escape_string($conn, $_POST['message']);
+    $sql = "INSERT INTO contact_messages (name, email, subject, message) 
+            VALUES ('$name', '$email', '$subject', '$message')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo 'success';
+    } else {
+        echo 'error: ' . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+}
+?>
